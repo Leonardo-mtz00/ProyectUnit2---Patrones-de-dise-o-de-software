9b89359303b53f8f2ce81b6dad942f0f70f8d7d6
Leonardo-mtz00 <l20120135@morelia.tecnm.mx>
2026-03-31 21:13:38 -0600

    Primer commit

---
+++ Proyecto.py
@@ -0,0 +1,8 @@
+## Proyecto Unidad 2 ##
+
+# Integrantes:
+# Francisco Alejandro Gaona Romero
+# Leonardo Martinez Gomez 
+
+# -----------------------------------------------------------------------------------------------------------
+
